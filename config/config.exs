3ff2e1b229b7f7f9819e1633d7bc80957b722c0b
Andrey Mukamolow <fobo66@protonmail.com>
2018-02-07 18:41:45 +0300

    Fixed Heroku config

--- config/config.exs
+++ config/config.exs
@@ -2,15 +2,11 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-db = URI.parse(System.get_env("DATABASE_URL"))
-dbCredentials = String.split(db.userinfo, ":")
-
 config :vuetaba, CommentsRepo,
   adapter: Ecto.Adapters.Postgres,
-  database: String.replace_prefix(db.path, "/", ""),
-  username: hd(dbCredentials),
-  password: tl(dbCredentials),
-  hostname: db.host
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 config :vuetaba, ecto_repos: [CommentsRepo]
 
