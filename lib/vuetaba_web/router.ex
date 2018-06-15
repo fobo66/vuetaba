a14810d7cc7f2a3ef0fcc7cf858d86c48b4d18a4
Andrey Mukamolow <fobo66@protonmail.com>
2018-06-16 00:17:17 +0300

    Fixed router

--- lib/vuetaba_web/router.ex
+++ lib/vuetaba_web/router.ex
@@ -1,34 +1,15 @@
 defmodule VuetabaWeb.Router do
   use VuetabaWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
-    plug Absinthe.Plug, schema: VuetabaWeb.Schema
+    plug(:accepts, ["json"])
   end
 
-  pipeline :graphiql do
-    plug Absinthe.Plug.GraphiQL, schema: VuetabaWeb.Schema
-  end
-
-  scope "/", VuetabaWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
-  # Other scopes may use custom stacks.
   scope "/api", VuetabaWeb do
-    pipe_through :api
-  end
+    pipe_through(:api)
 
-  scope "/graphiql", VuetabaWeb do
-    pipe_through :graphiql
+    forward("/", Absinthe.Plug, schema: VuetabaWeb.Schema)
+
+    forward("/graphiql", Absinthe.Plug.GraphiQL, schema: VuetabaWeb.Schema)
   end
 end
