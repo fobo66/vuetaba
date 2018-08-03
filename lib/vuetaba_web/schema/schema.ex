ac962ba48e6f5eb46fa978392dc93079bc860232
Andrey Mukamolow <fobo66@protonmail.com>
2018-08-03 21:53:40 +0300

    Added first query

--- lib/vuetaba_web/schema/schema.ex
+++ lib/vuetaba_web/schema/schema.ex
@@ -1,5 +1,13 @@
 defmodule VuetabaWeb.Schema do
   use Absinthe.Schema
-
   import_types(VuetabaWeb.Schema.Types)
+
+  alias VuetabaWeb.Resolvers
+
+  query do
+    @desc "Get all attachments"
+    field :attachments, list_of(:attachment) do
+      resolve &Resolvers.Attachments.load_all_attachments/3
+    end
+  end
 end
