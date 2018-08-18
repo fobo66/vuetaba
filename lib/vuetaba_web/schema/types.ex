bb79dda32e3b2519ef712fa76230a272186c14bf
Andrey Mukamolow <fobo66@protonmail.com>
2018-08-18 18:10:10 +0300

    Absinthe.Ecto integration

--- lib/vuetaba_web/schema/types.ex
+++ lib/vuetaba_web/schema/types.ex
@@ -1,23 +1,24 @@
 defmodule VuetabaWeb.Schema.Types do
   use Absinthe.Schema.Notation
+  use Absinthe.Ecto, repo: Vuetaba.Repo
 
   object :board do
     field :name, non_null(:string)
     field :tag, non_null(:string)
-    field :threads, list_of(:thread)
+    field :threads, list_of(:thread), resolve: assoc(:threads)
   end
 
   object :thread do
     field :name, :string
     field :message, :string
-    field :comments, list_of(:comment)
+    field :comments, list_of(:comment), resolve: assoc(:comments)
   end
 
   object :comment do
     field :name, :string
     field :comment, :string
     field :sage, :boolean
-    field :attachments, list_of(:attachment)
+    field :attachments, list_of(:attachment), resolve: assoc(:attachments)
   end
 
   object :attachment do
