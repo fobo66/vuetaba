62c0f8eabb1a883e553d533b3276eb5f86c03042
Andrey Mukamolov <fobo66@protonmail.com>
2019-02-16 14:19:27 +0300

    Define adapter in repo

--- lib/vuetaba/repo.ex
+++ lib/vuetaba/repo.ex
@@ -1,5 +1,7 @@
 defmodule Vuetaba.Repo do
-  use Ecto.Repo, otp_app: :vuetaba
+  use Ecto.Repo, 
+  otp_app: :vuetaba,
+  adapter: Ecto.Adapters.Postgres
 
   @doc """
   Dynamically loads the repository url from the
