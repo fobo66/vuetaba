cf529b99b98dd3bba7a415b3851e5817b4db2e61
Andrey Mukamolov <fobo66@protonmail.com>
2019-07-01 20:18:15 +0300

    Added test for deleting board

--- test/vuetaba_web/schema/mutations_test.exs
+++ test/vuetaba_web/schema/mutations_test.exs
@@ -47,4 +47,32 @@ defmodule VuetabaWeb.Schema.MutationsTest do
     %{"updateBoard" => result} = data
     Kronky.TestHelper.assert_equivalent_graphql(expected, result, board_fields())
   end
+
+  test "Delete board correct mutation" do
+    create_query = "mutation {
+      createBoard(
+        name: \"test\",
+        tag: \"t\"
+      ) {
+        id
+      }
+    }
+    "
+
+      {:ok, %{data: create_result}} = evaluate_graphql(create_query)
+
+      %{"createBoard" => id} = create_result
+
+      query = "mutation {
+        deleteBoard(
+          id: #{Integer.to_string(id["id"])}
+        )
+      }
+      "
+
+      assert {:ok, %{data: data}} = evaluate_graphql(query)
+      expected = id["id"]
+      %{"deleteBoard" => result} = data
+      assert expected === result
+    end
 end
