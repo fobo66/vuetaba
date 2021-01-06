b1894b777d7f6a704f305f3e3adfecdcfee13546
Andrey Mukamolov <fobo66@protonmail.com>
2021-01-06 23:43:41 +0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vuetaba.Mixfile do
   def project do
     [
       app: :vuetaba,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
